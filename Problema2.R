d01f26c29c401ddaa9730ee0d0cbd78edd2e5f08
Erik5376 <ehernandezteruel@gmail.com>
2025-02-18 09:50:49 +0100

    Update Problema2.R

--- Problema2.R
+++ Problema2.R
@@ -1,1 +1,18 @@
 #Codigo para problema 2
+
+mis_dades <- mtcars
+x <- mis_dades$cyl
+#freq abs
+ni <- table(x)
+barplot(ni)
+#freq rel
+fi <- ni/length(x)
+pie(fi)
+#freq acumulada
+Ni <- cumsum(ni)
+#freq rel acumulada
+Fi <- cumsum(fi)
+cbind(ni, fi, Ni, Fi)
+
+data <- mis_dades
+hist(data$mpg)2
